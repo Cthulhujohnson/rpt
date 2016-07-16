f271a1891ce2547f8d7499430196886d76df2af0
Jae <jaecarbary@gmail.com>
2016-07-16 17:52:10 +0000

    added posting ability and implemented it as news for Welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  
+  def index
+    @news = Post.all.order('id DESC')
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708214041) do
+ActiveRecord::Schema.define(version: 20160716162716) do
 
   create_table "adventurers", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20160708214041) do
 
   create_table "genders", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
